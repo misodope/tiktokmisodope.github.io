e5cd0211c359c55e0f6a4075ff25ddb0410b4416
Jerry Chen <misodope@cap-rx.com>
2021-03-20 21:13:06 -0400

    MOre updates

--- tiktok/src/components/Links.js
+++ tiktok/src/components/Links.js
@@ -3,6 +3,18 @@ import "./Links.scss";
 
 const linkData = [
   {
+    title: 'App Academy Open',
+    url: 'https://open.appacademy.io/signup/free?utm_source=partner&utm_medium=tiktok&utm_campaign=miso-dope&utm_content=open',
+  },
+  {
+    title: 'Candor',
+    url: 'https://candor.co/offers',
+  },
+  {
+    title: 'Lepow Portable Monitor',
+    url: 'https://tiktech.com/products/15-6-inch-lepow-full-hd-1080p-portable-led-monitor-with-hdmi-type-c',
+  },
+  {
     title: 'ASUS ProArt Monitors',
     url: 'https://bit.ly/tt-ASUSxmisodope',
   },
