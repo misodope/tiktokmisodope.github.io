03e028c774f6e9e6df5b39bd2d25cd368f513da7
Jerry Chen <misodope@cap-rx.com>
2022-03-09 22:50:06 -0500

    Update logo

--- tiktok/src/components/MainHeader.js
+++ tiktok/src/components/MainHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../images/misodope_logo_169.png";
+import logo from "@images/Head2022.png";
 import "./MainHeader.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
@@ -8,7 +8,7 @@ const MainHeader = () => {
   return (
     <div className="header">
       <div className="mobile-header">
-        <img className="w-30 h-30" src={logo} alt="logo" />
+        <img className="w-32 h-32 mb-5" src={logo} alt="logo" />
         <div className="text-xl tracking-wider">@MisoDope</div>
         <div className="text-sm flex flex-col items-center">
           <p>Digital Creator</p>
