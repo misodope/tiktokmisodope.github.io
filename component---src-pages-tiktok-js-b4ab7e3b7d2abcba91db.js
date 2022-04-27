ec3cf7d4f92810335bd409897fa38deca218fcf2
Jerry Chen <misodope@cap-rx.com>
2022-04-27 12:16:09 -0400

    Updates

---
+++ component---src-pages-tiktok-js-b4ab7e3b7d2abcba91db.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[132],{9196:function(t,e,s){s.r(e);var n=s(7294);e.default=function(){return(0,n.useEffect)((function(){window.location.href="https://tiktok.com/@misodope"}),[]),n.createElement("p",{className:"flex justify-center items-center font-bold text-lg animated pulse"},"Redirecting to @MisoDope TikTok...")}}}]);
+//# sourceMappingURL=component---src-pages-tiktok-js-b4ab7e3b7d2abcba91db.js.map
