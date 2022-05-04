ae031f97659e8e35d302ebe421cceb9c02f313a6
Jerry Chen <misodope@cap-rx.com>
2022-05-04 13:38:24 -0400

    add floating square background

--- tiktok/src/components/Container.js
+++ tiktok/src/components/Container.js
@@ -1,5 +1,25 @@
-import * as React from "react"
+import * as React from "react";
+
+import "./FloatingSquares.scss";
+
+const FloatingSquares = () => (
+  <ul class="circles">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+);
 
 export const Container = ({ children }) => (
-  <div className="container mx-auto">{children}</div>
-)
+  <div className="container mx-auto">
+    {children}
+    <FloatingSquares />
+  </div>
+);
