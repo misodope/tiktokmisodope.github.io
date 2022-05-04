47f98070f06dfdd3eda11f06429597017d5703f8
Jerry Chen <misodope@cap-rx.com>
2022-05-04 14:02:38 -0400

    fix square height

--- tiktok/src/components/Container.js
+++ tiktok/src/components/Container.js
@@ -1,14 +1,22 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./FloatingSquares.scss";
 
-const FloatingSquares = () => (
-  <ul className="circles">
-    {[...Array(10)].map((_, i) => (
-      <li key={i}></li>
-    ))}
-  </ul>
-);
+const FloatingSquares = () => {
+  const [pageHeight, setPageHeight] = useState(0);
+
+  useEffect(() => {
+    setPageHeight(document.body.clientHeight);
+  }, []);
+
+  return (
+    <ul className="circles" style={{ height: `${pageHeight}px` }}>
+      {[...Array(10)].map((_, i) => (
+        <li key={i}></li>
+      ))}
+    </ul>
+  );
+};
 
 export const Container = ({ children }) => (
   <div className="container mx-auto">
