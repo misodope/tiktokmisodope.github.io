3263aac1afc44fb7d8cd44beaab198d2ee0c2060
Jerry Chen <misodope@cap-rx.com>
2022-06-30 15:50:24 -0400

    configs

--- svelte-tok/svelte.config.js
+++ svelte-tok/svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
-const dev = process.env.NODE_ENV === "development";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -21,7 +19,7 @@ const config = {
     prerender: {
       default: true,
     },
-    appDir: "_app",
+    appDir: "app_",
   },
 };
 
