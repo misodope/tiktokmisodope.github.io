03d38ff904e916cc5f5c371a2eb8e5ce4d96d689
Jerry Chen <misodope@cap-rx.com>
2022-06-30 13:39:02 -0400

    add additional pages

---
+++ svelte-tok/src/routes/job-search.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    window.location.href = "https://docs.google.com/spreadsheets/d/1ipEvKRYzXIS3ERa9FE5Iw7appsNLAfJpelDEE4r9nus";
+  })
+</script>
+
+<div class="w-full h-screen flex flex-col justify-center items-center">
+  <p>Oh look I can center a div</p>
+  <p class=" font-bold text-lg animate-pulse">Loading Job Search Template...</p>
+</div>
