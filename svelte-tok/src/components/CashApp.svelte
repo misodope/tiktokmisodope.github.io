381e42d2d7da412854576ea40a43415b231b7a5b
Jerry Chen <misodope@cap-rx.com>
2022-07-31 14:46:11 -0400

    refactor

--- svelte-tok/src/components/CashApp.svelte
+++ svelte-tok/src/components/CashApp.svelte
@@ -1,83 +1,84 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import Loader from "../components/Loader.svelte";
+	import { onMount } from 'svelte';
+	import Loader from '../components/Loader.svelte';
 
-  const isDev = import.meta.env.DEV;
-  const appId = isDev ? import.meta.env.VITE_SQUARE_SANDBOX_APP_ID : import.meta.env.VITE_SQUARE_P_APP_ID;
-  const locationId = isDev ? import.meta.env.VITE_SQUARE_LOCATION_ID : import.meta.env.VITE_SQUARE_P_LOCATION_ID;
+	const isDev = import.meta.env.DEV;
+	const appId = isDev
+		? import.meta.env.VITE_SQUARE_SANDBOX_APP_ID
+		: import.meta.env.VITE_SQUARE_P_APP_ID;
+	const locationId = isDev
+		? import.meta.env.VITE_SQUARE_LOCATION_ID
+		: import.meta.env.VITE_SQUARE_P_LOCATION_ID;
 
-  let loading = true;
+	let loading = true;
 
-  interface CashAppPay {
-    addEventListener: Function;
-    attach: Function; 
-  }
+	interface CashAppPay {
+		addEventListener: Function;
+		attach: Function;
+	}
 
-  const renderPaymentCompleted = () => {
-    const cashAppElement = document.getElementById("cash-app-pay");
-    const lastChild = cashAppElement?.lastElementChild;
+	const renderPaymentCompleted = () => {
+		const cashAppElement = document.getElementById('cash-app-pay');
+		const lastChild = cashAppElement?.lastElementChild;
 
-    const pElement = document.createElement("p");
-    pElement.innerHTML = "Cash App Payment Complete!"
-    
-    lastChild && cashAppElement?.replaceChild(pElement, lastChild);
-  }
+		const pElement = document.createElement('p');
+		pElement.innerHTML = 'Cash App Payment Complete!';
 
-  const cashAppPayment = () => {
-    const payments = window.Square.payments(appId, locationId);
+		lastChild && cashAppElement?.replaceChild(pElement, lastChild);
+	};
 
-    const request = payments.paymentRequest({
-      countryCode: 'US',
-      currencyCode: 'USD',
-      total: {
-        amount: '1.00',
-        label: 'Total',
-      }
-    }); 
+	const cashAppPayment = async () => {
+		const payments = window.Square.payments(appId, locationId);
 
-    const options = {
-      redirectURL: window.location.href,
-      referenceId: "misodope-cash-app-pay"
-    }
+		const request = payments.paymentRequest({
+			countryCode: 'US',
+			currencyCode: 'USD',
+			total: {
+				amount: '1.00',
+				label: 'Total'
+			}
+		});
 
-    return payments.cashAppPay(request, options)
-      .then((cashAppPay: CashAppPay) => {
-        cashAppPay.addEventListener('ontokenization', (event: any) => {
-          const { tokenResult } = event.detail;
-          if (tokenResult.status === 'OK') {
-            renderPaymentCompleted();
-          }
-        });
+		const options = {
+			redirectURL: window.location.href,
+			referenceId: 'misodope-cash-app-pay'
+		};
 
-        loading = false;
+		try {
+			const cashAppPay: CashAppPay = await payments.cashAppPay(request, options);
 
-        return cashAppPay;
-      })
-      .then((cashAppPay: CashAppPay) => {
-        const cashAppButtonStyles = {
-          theme: "light",
-          values: "light",
-        }
+			cashAppPay.addEventListener('ontokenization', (event: any) => {
+				const { tokenResult } = event.detail;
+				if (tokenResult.status === 'OK') {
+					renderPaymentCompleted();
+				}
+			});
 
-        cashAppPay.attach('#cash-app-pay', cashAppButtonStyles);
+			if (cashAppPay) loading = false;
 
-        return cashAppPay;
-      })
-      .catch((err: Error) => {
-        console.error(err);
-      })
-  }
+			return cashAppPay;
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  onMount(() => {
-    cashAppPayment();
-  })
+	onMount(async () => {
+		const cashAppPay = await cashAppPayment();
+
+		const cashAppButtonStyles = {
+			theme: 'light',
+			values: 'light'
+		};
+
+		await cashAppPay?.attach('#cash-app-pay', cashAppButtonStyles);
+	});
 </script>
 
 <div class="mb-3 flex flex-col items-center">
-  {#if loading}
-    <Loader />
-  {:else}
-    <p class="text-amber-300 text-lg font-bold mb-[10px]">Get MisoDope Coffee! - $1</p>
-    <div id="cash-app-pay" />
-  {/if}
+	{#if loading}
+		<Loader />
+	{:else}
+		<p class="text-amber-300 text-lg font-bold mb-[10px]">Get MisoDope Coffee! - $1</p>
+		<div id="cash-app-pay" />
+	{/if}
 </div>
