9234716c355dc7f6120316f07e675f812e44dfd7
Jerry Chen <misodope@cap-rx.com>
2022-08-02 23:06:20 -0400

    Add cashapp test page

--- svelte-tok/src/routes/cash-app.svelte
+++ svelte-tok/src/routes/cash-app.svelte
@@ -3,9 +3,11 @@
 	import Socials from '../components/Social.svelte';
 	import Links from '../components/Links.svelte';
 	import Footer from '../components/Footer.svelte';
+	import CashApp from '../components/CashApp.svelte';
 </script>
 
 <Header />
 <Socials />
+<CashApp />
 <Links />
 <Footer />
