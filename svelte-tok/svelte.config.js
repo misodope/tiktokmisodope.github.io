19942c0aaa7158b53b57ea0ff325ebf13c7099db
Jerry Chen <misodope@cap-rx.com>
2023-10-05 01:15:14 -0700

    fix github pages setup

--- svelte-tok/svelte.config.js
+++ svelte-tok/svelte.config.js
@@ -1,28 +1,27 @@
-import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
-import path from "path"
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess([{ postcss: true }, vitePreprocess]),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess([{ postcss: true }, vitePreprocess]),
 
-  kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-    }),
-    paths: {
-      base: "",
-    },
-    prerender: {
-      entries: ["*"],
-    },
-    appDir: "_app",
-  },
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		paths: {
+			base: ''
+		},
+		prerender: {
+			entries: ['*']
+		},
+		appDir: '_app'
+	}
 };
 
 export default config;
